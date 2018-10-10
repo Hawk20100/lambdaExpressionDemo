e679fdba42c060ecc9f783f7f4df1e737a270562
Amr Ahmed <popsot_2010@hotmail.com>
2018-10-10 15:02:16 +0200

    Creating  Lambda Expression Demo

---
+++ lambdaExpressionDemo/LambdaExpressionInfo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lambdaExpressionDemo
+{
+    public class LambdaExpressionInfo
+    {
+        /*
+         * What is lambda expression?
+            -an anonymous method
+                * No access modifier.
+                * No name.
+                * No return statement. 
+          * Why do we use them?
+            -For Convenience.
+         */
+
+        // Lambda Expression Syntax
+        /*
+            args => Expression
+            ()=> ... no arguments
+            x => ... one arguments
+            (x, y, z) => ... Multiple arguments
+         */
+    }
+}
--- lambdaExpressionDemo/Program.cs
+++ lambdaExpressionDemo/Program.cs
@@ -10,6 +10,28 @@ namespace lambdaExpressionDemo
     {
         static void Main(string[] args)
         {
+            //number => number * number
+
+            // Func<int, int> square = number => number * number;
+            //const int factor = 5;
+            //Func<int, int> multipler = n => n * factor;
+            //var result = multipler(10);
+
+            var books = new BookRepository().GetBooks();
+
+            var cheapBooks = books.FindAll(b => b.Price < 10);
+
+            foreach (var book in cheapBooks)
+            {
+                Console.WriteLine(book.Title);
+            }
+
+            Console.ReadLine();
         }
+
+        //static bool IsCheaperThan10Dollars(Book book)
+        //{
+        //    return book.Price < 10;
+        //}
     }
 }
